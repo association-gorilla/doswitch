c7276f60aeccd8610bf59da3c1f7614eef033912
kaifumi <kaifumi1001@gmail.com>
2020-05-16 11:43:21 +0900

    add_top_verb #10

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,10 @@ class HomesController < ApplicationController
   # ログインユーザーのみ実行可能にする
   before_action :authenticate_user!
 
-  def top; end
+  def top
+    @important_verb = Verb.find_by(user_id: current_user.id, important: true)
+    @selected_verbs = Verb.where(user_id: current_user.id, selected: true, important: false)
+  end
 
   def about; end
 end
